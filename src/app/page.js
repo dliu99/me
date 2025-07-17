6de5379b7544903dad51143b8eb53991e274f136
devin <80857978+notdevin-1@users.noreply.github.com>
2025-07-17 00:08:53 -0700

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -21,22 +21,21 @@ export default function Home() {
           <ModeToggle/>
         </div>
       </header>
-      <p className="  text-left text-secondary">Hey, I'm Devin! I'm  
+      <p className="  text-left text-secondary">Hey, <b>I'm Devin</b>! I'm  
         <Tooltip>
           <TooltipTrigger><b className="px-1 rounded-md bg-slate-200 dark:bg-slate-800">17</b></TooltipTrigger>
           <TooltipContent>March 21, 2008</TooltipContent>
         </Tooltip>
           and currently a senior at <b>Monte Vista High School</b> (Danville, CA)</p>
-      <p className="text-secondary">i'm a developer, wrestler, soon-to-be eagle scout, and go to the gym (225 bench otw)</p>
+      <p className="text-secondary">i'm a developer, wrestler, soon-to-be eagle scout, and I go to the gym (225 bench otw)</p>
       <section className="">
           
           
           <h2 className="text-xl font-semibold mb-2">what im doing:</h2>
           <ul className="list-disc pl-5 space-y-1 text-secondary">
-          <li>building new things</li>
+          <li>side projects (like making this website less boring)</li>
           <li>winning hackathons at <UnderlineLink href="https://devpost.com/software/flamesense">los altos hacks</UnderlineLink>, <UnderlineLink href="https://devpost.com/software/callsense-jo4hg3">milpitas hacks</UnderlineLink>, and <UnderlineLink href="https://lu.ma/hacknight-at-github-07-08-25">hack night @ github</UnderlineLink></li>
-            <li>helping my teachers automate grading</li>
-            <li><UnderlineLink href="https://feed-fwd.org">feed forward</UnderlineLink> — food rescue org, 5k lbs+ produce donated <i>(ongoing)</i></li>
+            <li><UnderlineLink href="https://feed-fwd.org">feed forward</UnderlineLink> — food rescue org, 5k lbs+ produce donated</li>
             <li>social media — managed team of 2 pushing daily videos to 10k followers (20m views, $6k/m in &lt;2months) </li>
             <li>ecom — ran stores, built & sold tools for other store owners to find & contact affiliates</li>
           </ul>
